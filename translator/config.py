a10a0d81304074d6a8b95063514c70513f83bdd1
AoAO <64264590+Leo-AO-99@users.noreply.github.com>
2024-12-10 21:22:24 -0500

    Train Code update (#7)
    * train code
    
    * update
    
    * update

--- translator/config.py
+++ translator/config.py
@@ -11,4 +11,4 @@ class ModelConfig(BaseModel):
     src_vocab_size: int = Field(default=10000,description="source vocabulary size")
     tgt_vocab_size: int = Field(default=10000,description="target vocabulary size")
     max_seq_len: int = Field(default=256, description="maximum sequence length")
-    max_batch_size: int = Field(default=32, description="maximum batch size")
+    max_batch_size: int = Field(default=64, description="maximum batch size")
